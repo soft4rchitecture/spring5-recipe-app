e7c75f4ba7a48915f71dd772e4c62872240a428b
dude <monfollet.romain.pro@gmail.com>
2021-12-12 22:12:27 +0100

    poor m'an's exercice of Spring Data JPA Query methods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,35 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import lombok.AllArgsConstructor;
 
 /**
  * Created by jt on 6/1/17.
  */
+@AllArgsConstructor
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @RequestMapping({ "", "/", "/index" })
+    public String getIndexPage() {
+
+        Optional<Category> category = this.categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("category description: " + category.get().getDescription());
+        System.out.println("unitOfMeasure description: " + unitOfMeasure.get().getDescription());
+
         return "index";
     }
 }
