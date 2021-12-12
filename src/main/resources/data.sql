15348169b6ba147dac383ded416ae73d43207104
dude <monfollet.romain.pro@gmail.com>
2021-12-12 19:39:55 +0100

    Database Initialization with Spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+-- Categories
+INSERT INTO category (name)  VALUES ('American')
+INSERT INTO category (name)  VALUES ('Italian')
+INSERT INTO category (name) VALUES ('Mexican')
+INSERT INTO category (name) VALUES ('Fast Food')
+
+-- Units of Measure
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon')
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon')
+INSERT INTO unit_of_measure (description) VALUES ('Cup')
+INSERT INTO unit_of_measure (description) VALUES ('Pinch')
+INSERT INTO unit_of_measure (description) VALUES ('Ounce')
