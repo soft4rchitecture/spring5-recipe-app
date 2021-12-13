10fb9e2c25b13f1a1a81c2e23a9ed9dc1cac483e
dude <monfollet.romain.pro@gmail.com>
2021-12-13 21:42:11 +0100

    Create Visit Entity

--- src/main/java/guru/springframework/domain/Pet.java
+++ src/main/java/guru/springframework/domain/Pet.java
@@ -1,11 +1,15 @@
 package guru.springframework.domain;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -28,4 +32,6 @@ public class Pet extends BaseEntity {
   @Column(name = "birth_date")
   private LocalDate birthDate;
 
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+  private Set<Visit> visits = new HashSet<>();
 }
---
+++ src/main/java/guru/springframework/domain/Visit.java
@@ -0,0 +1,26 @@
+package guru.springframework.domain;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Value;
+
+@Entity
+@Table(name = "visits")
+@Value
+public class Visit extends BaseEntity {
+
+  @Column(name = "date")
+  LocalDate date;
+  @Column(name = "description")
+  String description;
+  @ManyToOne
+  @JoinColumn(name = "pet_id")
+  Pet pet;
+
+}
